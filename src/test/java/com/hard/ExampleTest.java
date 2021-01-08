7139137fb924efa6566f4ae4000432650f316405
hard <harleydav@mail.ru>
2021-01-08 12:31:18 +0300

    ExampleTest

---
+++ src/test/java/com/hard/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.hard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+    @Test
+    public void shouldShowAssertion() {
+        assertEquals(4, 2 + 2);
+    }
+}
