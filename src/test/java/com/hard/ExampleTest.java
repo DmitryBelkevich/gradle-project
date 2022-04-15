31b8d7d0fcf749482d5f6918c1f84d0324f4771f
hard <harleydav@mail.ru>
2022-04-15 12:18:46 +0300

    test

--- src/test/java/com/hard/ExampleTest.java
+++ src/test/java/com/hard/ExampleTest.java
@@ -1,11 +1,14 @@
 package com.hard;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("Example test")
 public class ExampleTest {
     @Test
+    @DisplayName("should show assertion")
     public void shouldShowAssertion() {
         assertEquals(4, 2 + 2);
     }
